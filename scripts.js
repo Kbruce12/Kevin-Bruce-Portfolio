cc3376ecd64f13bf7c29d931310a0a7978cdef3e
KevinBruce <kevinlawbruce@gmail.com>
2022-08-07 21:07:03 -0400

    media Qs

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const overLay = document.querySelector('.overlay');
 const navEl = document.querySelectorAll('.sticky ul li');
-const homeEl = document.querySelector('.home')
-const aboutEl = document.querySelector('.aboutNav')
-const projectsEl = document.querySelector('.projectsNav')
-const newsEl = document.querySelector('.newsNav')
-const contactEl = document.querySelector('.contactNav')
-const homePage = document.querySelector('.creator-heading')
-const aboutPage = document.querySelector('.about')
-const projectPage = document.querySelector('.projects')
-const newsPage = document.querySelector('.news')
-const contactPage = document.querySelector('.get-in-touch')
+const homeEl = document.querySelector('.home');
+const aboutEl = document.querySelector('.aboutNav');
+const projectsEl = document.querySelector('.projectsNav');
+const newsEl = document.querySelector('.newsNav');
+const contactEl = document.querySelector('.contactNav');
+const homePage = document.querySelector('.creator-heading');
+const aboutPage = document.querySelector('.about');
+const projectPage = document.querySelector('.projects');
+const newsPage = document.querySelector('.news');
+const contactPage = document.querySelector('.get-in-touch');
+const inTouchEl = document.querySelector('.button');
 
 
 
@@ -29,39 +30,48 @@ navEl.forEach(list_item => {
     })
 })
 
-// homeEl.addEventListener('click', () => {
-//     homePage.style.visibility = 'visible';
-//     aboutPage.style.visibility = 'hidden';
-//     projectPage.style.visibility = 'hidden';
-//     newsPage.style.visibility = 'hidden';
-//     contactPage.style.visibility = 'hidden';
-// })
-// aboutEl.addEventListener('click', () => {
-//     homePage.style.visibility = 'hidden';
-//     aboutPage.style.visibility = 'visible';
-//     projectPage.style.visibility = 'hidden';
-//     newsPage.style.visibility = 'hidden';
-//     contactPage.style.visibility = 'hidden';
-// })
-// projectsEl.addEventListener('click', () => {
-//     homePage.style.visibility = 'hidden';
-//     aboutPage.style.visibility = 'hidden';
-//     projectPage.style.visibility = 'visible';
-//     newsPage.style.visibility = 'hidden';
-//     contactPage.style.visibility = 'hidden';
-// })
-// newsEl.addEventListener('click', () => {
-//     homePage.style.visibility = 'hidden';
-//     aboutPage.style.visibility = 'hidden';
-//     projectPage.style.visibility = 'hidden';
-//     newsPage.style.visibility = 'visible';
-//     contactPage.style.visibility = 'hidden';
-// })
-// contactEl.addEventListener('click', () => {
-//     homePage.style.visibility = 'hidden';
-//     aboutPage.style.visibility = 'hidden';
-//     projectPage.style.visibility = 'hidden';
-//     newsPage.style.visibility = 'hidden';
-//     contactPage.style.visibility = 'visible';
-// })
+homeEl.addEventListener('click', () => {
+    homePage.style.visibility = 'visible';
+    aboutPage.style.visibility = 'hidden';
+    projectPage.style.visibility = 'hidden';
+    newsPage.style.visibility = 'hidden';
+    contactPage.style.visibility = 'hidden';
+})
+aboutEl.addEventListener('click', () => {
+    homePage.style.visibility = 'hidden';
+    aboutPage.style.visibility = 'visible';
+    projectPage.style.visibility = 'hidden';
+    newsPage.style.visibility = 'hidden';
+    contactPage.style.visibility = 'hidden';
+})
+projectsEl.addEventListener('click', () => {
+    homePage.style.visibility = 'hidden';
+    aboutPage.style.visibility = 'hidden';
+    projectPage.style.visibility = 'visible';
+    newsPage.style.visibility = 'hidden';
+    contactPage.style.visibility = 'hidden';
+})
+newsEl.addEventListener('click', () => {
+    homePage.style.visibility = 'hidden';
+    aboutPage.style.visibility = 'hidden';
+    projectPage.style.visibility = 'hidden';
+    newsPage.style.visibility = 'visible';
+    contactPage.style.visibility = 'hidden';
+})
+contactEl.addEventListener('click', () => {
+    homePage.style.visibility = 'hidden';
+    aboutPage.style.visibility = 'hidden';
+    projectPage.style.visibility = 'hidden';
+    newsPage.style.visibility = 'hidden';
+    contactPage.style.visibility = 'visible';
+})
+
+inTouchEl.addEventListener('click', () => {
+    homePage.style.visibility = 'hidden';
+    aboutPage.style.visibility = 'hidden';
+    projectPage.style.visibility = 'hidden';
+    newsPage.style.visibility = 'hidden';
+    contactPage.style.visibility = 'visible';
+})
+
 
